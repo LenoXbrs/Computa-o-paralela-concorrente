a2cb42d76ca31ea5b88a1dbc19fe47372576f721
Luis Eduardo <banedycraft@gmail.com>
2023-12-08 22:03:47 -0300

    first commit

--- av3/src/av3questao2/Ecommerce.java
+++ av3/src/av3questao2/Ecommerce.java
@@ -11,11 +11,9 @@ public class Ecommerce {
 		String serverUrl = "https://belmondojr.dev/compra.php?";
 
 		try {
-			// Dados simulados da compra
 			String[] produtos = { "Item1", "Item2" };
 			double[] quantidades = { 10.50, 20.75 };
 
-			// Enviar dados para o servidor e obter a resposta
 			String resposta = enviarCompra(serverUrl, produtos, quantidades);
 			System.out.println("Resposta do servidor: " + resposta);
 
@@ -25,21 +23,17 @@ public class Ecommerce {
 	}
 
 	private static String enviarCompra(String serverUrl, String[] produtos, double[] quantidades) throws Exception {
-		// Construir a URL com os parâmetros da compra
 		StringBuilder urlWithParams = new StringBuilder(serverUrl);
 		for (int i = 0; i < produtos.length; i++) {
 			urlWithParams.append("&products[]=").append(produtos[i]);
 			urlWithParams.append("&values[]=").append(quantidades[i]);
 		}
 
-		// Criar URL e abrir a conexão
 		URL url = new URL(urlWithParams.toString());
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-		// Configurar método de requisição e cabeçalhos
 		connection.setRequestMethod("GET");
 
-		// Obter a resposta do servidor
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
 			StringBuilder resposta = new StringBuilder();
 			String linha;
